5cd3c0d9a37df6373e7ab4912ac84740824afaec
sara526 <71785160+sara526@users.noreply.github.com>
2025-08-20 21:06:01 +0300

    add search input

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 var productName = document.getElementById("productName");
 var productPrice = document.getElementById("productPrice");
 var productCat = document.getElementById("productCategory");
-var productDesc = document.getElementById("productDesc");
- 
+var productDesc = document.getElementById("productDesc"); 
+
 var productsContainer = [];
+
+if(localStorage.getItem("myproject")!=null){
+    productsContainer=JSON.parse(localStorage.getItem("myproject"))
+    displayProducts(productsContainer)
+}else{
+    productsContainer=[];
+}
 
 function addProduct(){
     var product = {
@@ -13,9 +20,38 @@ function addProduct(){
         desc: productDesc.value,
     }
     productsContainer.push(product);
-    displayProducts(); // hoisted func 
-    clear();
+    localStorage.setItem("myproject",JSON.stringify(productsContainer)) //localStorage before displayproduct()
+    displayProducts(productsContainer); // hoisted func 
+    clear();  // hoisted func
 }
+
+
+function displayProducts(list){
+    var snadok = ``
+    for (let i = 0; i < list.length; i++){
+        snadok += `
+        <tr>
+            <td>${i}</td>
+            <td>${list[i].name}</td> 
+            <td>${list[i].price}</td>
+            <td>${list[i].cat}</td>
+            <td>${list[i].desc}</td>
+        </tr>`
+        
+    }
+    document.getElementById("tbody").innerHTML = snadok;
+}
+
+function search(searchterm){
+    var searchResult = [];
+    for (let i = 0; i<productsContainer.length; i++){
+        if (productsContainer[i].name.toLowerCase().includes(searchterm.toLowerCase())== true){
+            searchResult.push(productsContainer[i]);
+        }
+    } 
+    displayProducts(searchResult);
+}
+
 
 function clear(){
     productName.value = '',
@@ -23,20 +59,3 @@ function clear(){
     productCat.value = '',
     productDesc.value = ''
 }
-
-function displayProducts(){
-    var snadok = ``
-    for (let i = 0; i < productsContainer.length; i++){
-        snadok += `
-        <tr>
-            <td>${$i}</td>
-            <td>${productsContainer[i].name}</td> 
-            <td>${productsContainer[i].price}</td>
-            <td>${productsContainer[i].cat}</td>
-            <td>${productsContainer[i].desc}</td>
-        </tr>`
-        
-    }
-    document.getElementById("tbody").innerHTML = snadok;
-}
-
